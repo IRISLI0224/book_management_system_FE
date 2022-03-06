76145658e6d06cbc227b2fec34f2e38234663aff
IRISLI0224 <38321032+IRISLI0224@users.noreply.github.com>
2022-03-07 03:34:43 +1100

    change database link

--- src/config/backendApi.js
+++ src/config/backendApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080/api/v1'
+    ? 'https://desolate-everglades-87987.herokuapp.com/api/v1/'
     : 'https://desolate-everglades-87987.herokuapp.com/api/v1/';
 
 const backendApi = axios.create({
