f3500fa6ef1c91c0c7e6a02474840281fc322d62
IRISLI0224 <38321032+IRISLI0224@users.noreply.github.com>
2022-03-06 18:52:39 +1100

    build API configs in FE

---
+++ src/config/book.js
@@ -0,0 +1,95 @@
+//All API about user
+import backendApi from "./backendApi";
+
+const API_GET_BOOK = "/books";
+const API_GET_ALL_BOOKS = "/books";
+const API_CREATE_BOOK = "/create/book";
+const API_UPDATE_BOOK = "/books";
+const API_DELETE_BOOK = "/books";
+const API_BORROW_BOOK = "/books/borrow";
+
+export const getAllBooks = async (id) => {
+  const url = `${API_GET_ALL_BOOKS}`;
+  try {
+    const response = await backendApi.get(url);
+    return response.data;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+export const getBookById = async (id) => {
+  const url = `${API_GET_BOOK}/${id}`;
+  try {
+    const response = await backendApi.get(url);
+    return response.data;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+export const CreateBook = async (name, author, categories) => {
+  const data = {
+    name,
+    author,
+    categories,
+  };
+  try {
+    const res = await backendApi
+      .post(API_CREATE_BOOK, data)
+      .catch(function (e) {
+        if (e.response) {
+          return e.response;
+        }
+      });
+    return res;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+export const UpdateBookById = async (id, name, author, categories) => {
+  const data = {
+    name,
+    author,
+    categories,
+  };
+  try {
+    const res = await backendApi
+      .put(`${API_UPDATE_BOOK}/${id}`, data)
+      .catch(function (e) {
+        if (e.response) {
+          return e.response;
+        }
+      });
+    return res;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+export const BorrowBook = async (bookid, userid) => {
+  const url = `${API_BORROW_BOOK}/${bookid}/${userid}`;
+  try {
+    const response = await backendApi.put(url);
+    return response.data;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+export const DeleteBook = async (id) => {
+  const url = `${API_DELETE_BOOK}/${id}`;
+  try {
+    const response = await backendApi.delete(url);
+    return response.data;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
---
+++ src/config/user.js
@@ -0,0 +1,40 @@
+//All API about user
+import backendApi from './backendApi';
+
+const API_GET_USER = '/users';
+const API_LOGIN_URL = '/login';
+const API_REGISTER_URL = '/register';
+
+export const getUserById = async (id) => {
+  const url = `${API_GET_USER}/${id}`;
+  const response = await backendApi.get(url);
+  return response.data;
+};
+
+export const UserLogin = async (email, password) => {
+  const data = {
+    email,
+    password,
+  };
+
+  const res = await backendApi.post(API_LOGIN_URL, data).catch(function (e) {
+    if (e.response) {
+      return e.response;
+    }
+  });
+  return res;
+};
+
+export const UserRegister = async (email, password) => {
+  const data = {
+    email,
+    password,
+  };
+
+  const res = await backendApi.post(API_REGISTER_URL, data).catch(function (e) {
+    if (e.response) {
+      return e.response;
+    }
+  });
+  return res;
+};
