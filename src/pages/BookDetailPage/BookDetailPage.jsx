e77724576841dbc83787bf35a53b32c2f41a5019
IRISLI0224 <38321032+IRISLI0224@users.noreply.github.com>
2022-03-06 18:29:48 +1100

    build book detail page and deal with form

--- src/pages/BookDetailPage/BookDetailPage.jsx
+++ src/pages/BookDetailPage/BookDetailPage.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import One from "../../components/Header";
-
+import { Button } from "../../components/Button";
+import BookCoverImg from "../../assets/img/bookCover1.png";
+import { Link } from "react-router-dom";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 const Container = styled.div`
   height: auto;
@@ -16,29 +19,175 @@ const MainPanel = styled.div`
   height: auto;
   margin: auto;
   text-align: center;
-  width: 80%;
-  background-image: -webkit-linear-gradient(-90deg, #5e85fc 0%, #c46dfd 100%);
+  width: 95%;
   border-radius: 10px;
 `;
 
-const Title = styled.h1`
-  margin-top: 100px;
-  color: #e9e9e9;
+const Form = styled.div`
+  padding: 5px;
+  height: 350px;
+  margin: auto;
+  text-align: center;
+  width: 100%;
+  border-radius: 10px;
+  background: #f0edeb;
 `;
 
-const SubTitle = styled.h2`
-  color: #e9e9e9;
+const Text = styled.div`
+  color: #5c4c4c;
+  font-size: 20px;
+`;
+
+const BookCover = styled.img`
+  height: 200px;
+  width: auto;
+  margin-top: 50px;
+`;
+
+const Input = styled.input`
+  margin-left: 20px;
+  opacity: 0.7;
+  @media (max-width: 768px) {
+    width: 50%;
+    margin-left: 10px;
+  }
+`;
+
+const InputPanel = styled.div`
+  margin: auto;
+  justify-content: center;
+  padding: 20px;
+  display: flex;
+  margin-bottom: 10px;
+`;
+
+const InputInnerPanel = styled.div`
+  margin: auto;
+  justify-content: center;
+  padding: 20px;
+  display: flex;
+`;
+
+const Fields = styled.div`
+  margin: auto;
+  justify-content: center;
+  padding: 20px;
+  display: flex;
+  margin-bottom: 10px;
+  width: 60%;
+`;
+
+const InputFields = styled.div`
+  margin: auto;
+  padding: 10px;
+  display: flex;
+  margin-bottom: 10px;
+  flex-direction: column;
+`;
+
+const ButtonPanel = styled.div`
+  margin: auto;
+  padding: 20px;
+  display: flex;
+  margin-bottom: 10px;
+  flex-direction: column;
 `;
 
 const BookDetailPage = () => {
+  const [borrowed, setBorrowed] = useState(false);
+  const [name, setName] = useState();
+  const [author, setAuthor] = useState();
+  const [categories, setCategories] = useState();
+
+  const handleOnBlur = (event) => {
+    const { value, name } = event.target;
+  };
+  const handleDataChange = (event) => {
+    const { value, name } = event.target;
+    switch (name) {
+      case "name": {
+        setName(value);
+        return "name";
+      }
+      case "author": {
+        setAuthor(value);
+        return "author";
+      }
+      case "categories": {
+        setCategories(value);
+        return "categories";
+      }
+      default:
+        return "";
+    }
+  };
+
+  const handleSubmit = () => {
+    //test console
+    console.log(name);
+    console.log(author);
+    console.log(categories);
+
+  };
+
   return (
     <Container>
       <MainPanel>
-        <Title>Book DetailPage</Title>
-        <One />
+        <Header />
+        <Form>
+          <Text>Book detail information</Text>
+          <Fields>
+            <BookCover src={BookCoverImg} />
+            <InputFields>
+              <br />
+              <InputPanel>
+                <div>Name:</div>
+                <Input
+                  name="name"
+                  onChange={handleDataChange}
+                  onBlur={handleOnBlur}
+                />
+              </InputPanel>
+              <InputPanel>
+                <div>Author:</div>
+                <Input
+                  name="author"
+                  onChange={handleDataChange}
+                  onBlur={handleOnBlur}
+                />
+              </InputPanel>
+              <InputPanel>
+                <div>Categories:&nbsp;&nbsp;</div>
+                <select
+                  name="categories"
+                  onChange={handleDataChange}
+                  onBlur={handleOnBlur}
+                >
+                  <option value="Horror">Horror</option>
+                  <option value="Drama">Drama</option>
+                  <option value="Fiction">Fiction</option>
+                </select>
+              </InputPanel>
+            </InputFields>
+            <ButtonPanel>
+              <InputPanel>
+                {borrowed ? (
+                  <div>Status:&nbsp;&nbsp;Borrowed by User A </div>
+                ) : (
+                  <div>Status:&nbsp;&nbsp;In stock&nbsp;&nbsp;</div>
+                )}
+              </InputPanel>
+              {borrowed ? <Button>Return the book</Button> : <br />}
+              <br />
+              <Button onClick={handleSubmit}>Submit</Button>
+              <br />
+              <Button>Delete</Button>
+            </ButtonPanel>
+          </Fields>
+        </Form>
+        <Footer />
       </MainPanel>
     </Container>
   );
 };
-
 export default BookDetailPage;
